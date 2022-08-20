c7e90ca4e4baa65a617221ded19311d911820553
EstelleR <roges.estelle@gmail.com>
2022-08-20 17:12:01 +0200

    signInUp responsiveness done

--- src/pages/UserCorner/SignInUp/SignInUpStyle.jsx
+++ src/pages/UserCorner/SignInUp/SignInUpStyle.jsx
@@ -4,6 +4,7 @@ export const SignInUpContent = styled("div")`
   padding: 2em;
   display: flex;
   justify-content: space-evenly;
+  text-align: start;
 
   h2 {
     text-align: center;
@@ -12,37 +13,61 @@ export const SignInUpContent = styled("div")`
     margin-bottom: 1em;
   }
 
-.signInArea,
-.signUpArea {
-  margin: 1em 3em;
-  width: 100%;
+  .signInArea,
+  .signUpArea {
+    margin: 1em 3em;
+    width: 100%;
 
-  .form {
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
+    .form {
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
 
-    form {
-      width: 100%;
-      max-width: 400px;
+      form {
+        width: 100%;
+        max-width: 400px;
+      }
     }
   }
-}
 
-hr {
-  border: 1px dashed #ADD09F;
-  margin: 0;
-}
-
-.signUpBtn {
-  text-align: end;
-}
-
-button {
-  margin-top: 0;
-  a {
-    color: white;
+  hr {
+    border: 1px dashed #add09f;
+    margin: 0;
   }
-}
 
+  .signUpBtn {
+    text-align: end;
+  }
+
+  button {
+    margin-top: 0;
+    a {
+      color: white;
+    }
+  }
+
+  @media screen and (max-width: 992px) {
+    .signInArea,
+    .signUpArea {
+      margin: 0;
+      width: 100%;
+    }
+
+    .signInArea {
+      margin-right: 3em;
+    }
+    .signUpArea {
+      margin-left: 3em;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    .signInArea,
+    .signUpArea {
+      margin: 0;
+      margin-bottom: 2em;
+      width: 100%;
+    }
+  }
 `;
